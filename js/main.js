8926e78b531f28753f15f684524b46c718f47456
pablo <pablo.lopez.santori@gmail.com>
2017-04-02 21:56:04 +0200

    Added some basic boundries

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
 var gravity = 0.9; // Constante
-var velocity = 12; // Velocidad. Disminuye por la gravedad.
+var velocity = 9; // Velocidad. Disminuye por la gravedad.
 var position = 210; // posicion del pajaro
 var fps = 40;
-
-var test = 666;
 
 $(document).ready(function() {   
    
    setInterval(mainloop, fps);    //Actualiza la posición del pajaro
    
-   $("body").on("click", function(){
-      velocity = 12;
-   })    
+   
 })
 
 function mainloop() {
@@ -23,9 +19,28 @@ function mainloop() {
 
    // Aquí los pasamos el valor de la posicion en cada momento al CSS.
    $(".bird").css({"top": position + "px"}) 
+
+   //Límites
+   if (position < 32 || position > 395){
+      gravity = 0; velocity = 0;
+      noAnimation()
+      
+   }
 }
 
+// Funcion para saltar
+$(document).on("click", function(){
+      velocity = 12;
+})    
 
-if (position < 0){
-   console.log(test)
+//función para desactivar animaciones css
+
+function noAnimation (){
+   $("#container-floor").css({"-webkit-animation-play-state": 'paused'});
+   $("#container-sky").css({"-webkit-animation-play-state": 'paused'});
+   $(".div-obstacle-top").css({"-webkit-animation-play-state": 'paused'});
+   $(".div-obstacle-bottom").css({"-webkit-animation-play-state": 'paused'});
+   $(".ceiling").css({"-webkit-animation-play-state": 'paused'});
+   $(".bird").css({"-webkit-animation-play-state": 'paused'})
 }
+
