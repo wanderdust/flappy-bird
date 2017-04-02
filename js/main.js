f28dfdd36fb45f10ae7323c36eef1e842fae4287
pablo <pablo.lopez.santori@gmail.com>
2017-04-02 18:50:54 +0200

    added clicking to jump

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
-var gravity = ((Math.pow(1.1),2)-1); // Fuerza exponencial que tira hacia abajo
-var velocity = 15; // Numero constante
-var position = 220; // posicion del pajaro
+var gravity = 0.9; // Constante
+var velocity = 12; // Velocidad. Disminuye por la gravedad.
+var position = 210; // posicion del pajaro
 
 $(document).ready(function() {   //se ejecuta 60 veces por segundo
    setInterval(mainloop, 60);    //Actualiza la posición del pajaro
-});
+   
+   $("body").on("click", function(){
+      velocity = 12;
+   })
+
+})
+
 
 function mainloop() {
    var player = $(".bird");
    
    velocity -= gravity;
    position -= velocity; 
-   console.log(position)
+   console.log(velocity)
 
    $(".bird").css({"top": position + "px"})
    //Aquí los calculos que ejecuten la parábola.
 
    // Aquí los pasamos el valor de la posicion en cada momento al CSS.
    // Se actualizará con cada intervalo.
+
+  
 }
+
+ 
