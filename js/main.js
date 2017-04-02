992d6744f2dc1d75443eb8cfb9bdf83dd87fa6ae
pablo <pablo.lopez.santori@gmail.com>
2017-04-02 20:00:08 +0200

    Cleaned up js a little bit

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 var gravity = 0.9; // Constante
 var velocity = 12; // Velocidad. Disminuye por la gravedad.
 var position = 210; // posicion del pajaro
+var fps = 40;
 
-$(document).ready(function() {   //se ejecuta 60 veces por segundo
-   setInterval(mainloop, 60);    //Actualiza la posición del pajaro
+var test = 666;
+
+$(document).ready(function() {   
+   
+   setInterval(mainloop, fps);    //Actualiza la posición del pajaro
    
    $("body").on("click", function(){
       velocity = 12;
-   })
-
+   })    
 })
-
 
 function mainloop() {
    var player = $(".bird");
    
    velocity -= gravity;
    position -= velocity; 
-   console.log(velocity)
-
-   $(".bird").css({"top": position + "px"})
-   //Aquí los calculos que ejecuten la parábola.
+   console.log();
 
    // Aquí los pasamos el valor de la posicion en cada momento al CSS.
-   // Se actualizará con cada intervalo.
-
-  
+   $(".bird").css({"top": position + "px"}) 
 }
 
- 
+
+if (position < 0){
+   console.log(test)
+}
