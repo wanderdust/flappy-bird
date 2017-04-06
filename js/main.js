f387823ae3537279d3ae214befb4c24bb44bc69c
pablo <pablo.lopez.santori@gmail.com>
2017-04-06 18:06:40 +0200

    When bird collides it falls to the floor

--- js/main.js
+++ js/main.js
@@ -2,76 +2,78 @@ var gravity = 1; // Constante
 var velocity = 9; // Velocidad. Disminuye por la gravedad.
 var position = 210; // posicion del pajaro
 var fps = 45;
-
 var velocityY = 0; // Variable para darle nuevos valores a velocidad
-//var random = 2000; //Numero aleatorio para generar obstaculos
 var obstacleCount = 44;
 var j = -1;
 var i = 0;
+var fall = 0; // Pasa a ser -1 cuando choca con un obstaculo y así se pone la velocidad a 0 en la funcion jump()
 
 $(document).ready(function() {   
    
    var gameInterval = setInterval(mainloop, fps);    //Actualiza la posición del pajaro en cada frame
-   //setInterval(obstacleGenerator, random) //Opcion para crear los obstaculos
-
-
 });
 
 function mainloop() {
    var player = $(".bird");
-   
-   
+      
    velocity -= gravity;
    position -= velocity; 
    
-   
-
    // Aqui los pasamos el valor de la posicion en cada momento al CSS.
    player.css({"top": position + "px"}) 
+   
+   // Funcion para que el pájaro salte
+   jump();
 
+   // Establece los limites en el mapa
+   offLimits();
+
+   //Bucle para generar obstaculos cada 45fps
+   obstacleTimer();
+   
+   //Elimina los obstaculos que se han salido del mapa
+   obstacleDelete();
+   
+   //Detecta cuando bird ha hecho colision
+   isCollide();
+
+   addSpeed()
+   
+};
+
+
+
+// Funcion para saltar. Si toca el suelo deja de funcionar.
+   function jump(){
+      $(document).on("click", function(){
+         if(position<397 && fall === 0){
+            velocity = velocityY;
+         }else if(position >= 397 && fall === -1){
+            velocityY = -4;
+
+         }
+      }); 
+   }
+
+// Establece los limites en el mapa
+function offLimits(){
    //Limites 
-      // 1. Limites suelo-Game over
+      // 1. Limites suelo-Game over 
    if (position >= 397){
       gravity = 0; 
       velocity = 0;
       noAnimation(); 
       position=397;
-      obstacleCount = 46;//Para el generador de obstáculos
+      obstacleCount = 66;//PARA (stop) el generador de obstáculos
 
-
-   };
       // 2. Limites techo-rebote abajo
-   if(position < 35){
+   } else if(position < 35){
       velocityY = -4;
       velocity = velocityY;
    } else {
       velocityY = 12;
    };
-
-   // Funcion para saltar. Si toca el suelo deja de funcionar.
-   $(document).on("click", function(){
-      if(position<397){
-         velocity = velocityY;
-      }else(velocity = 0)
-   }); 
-
-   //Bucle para generar obstaculos
-   
-   obstacleCount++;
-   if (obstacleCount === 45){
-      obstacleGenerator();
-      obstacleCount = 0;
-
-   }
-   
-   obstacleDelete();
-   //console.log($(".obstacle-animated").position().left)
-
-   isCollide();
-   
-};
-
-   
+}
 
 //funcion para desactivar animaciones css
 
@@ -83,7 +85,9 @@ function noAnimation (){
    $(".bird").css({"-webkit-animation-play-state": 'paused'})
 };
 
-// Funcion que generalos obstaculos entre 1 y 4 segundos
+
+
+// Aqui se crean los nuevos divs de obstaculos de diferentes tamanos
 function obstacleGenerator (){
    i = j+1;
 
@@ -99,6 +103,17 @@ function obstacleGenerator (){
    j++;
 
 };
+
+//Bucle de 0 a 45 que ejecuta la funcion obstacleGenerator cada 45 fps
+
+function obstacleTimer(){
+   obstacleCount++;
+   if (obstacleCount === 65){
+      obstacleGenerator();
+      obstacleCount = 0;
+
+   }
+}
 
 
 //Funcion que elimina los obstaculos cuando desparecen de la pantalla
@@ -122,23 +137,32 @@ function isCollide() {
    if( 60 < div.position().left + 90 &&
        60 + bird.width() > div.position().left &&
        bird.position().top < ot.position().top + ot.height() + 30 &&
-       bird.height() + bird.position().top > 0
-      ){
-      console.log("hoorray")
+       bird.height() + bird.position().top > 0) {
+
+      fallDown();
    }     
    
    //Detector de colisiones obstacle-bottom
    if (60 < div.position().left + 90 &&
-   60 + bird.width() > div.position().left &&
-   bird.position().top > 388 - ob.height() &&
-   bird.height() + bird.position().top < 388) {
-    console.log("hoooorray2")
-}
+      60 + bird.width() > div.position().left &&
+      bird.position().top > 388 - ob.height() &&
+      bird.height() + bird.position().top < 388) {
+   
+      fallDown();
+
+   }
     
 }
 
-function gameOver(){
-   
+// Cuando el pájaro se choca cae al suelo
+function fallDown(){
+   noAnimation();
+   fall = -1
+   velocityY = false;
+   obstacleCount = 66; //para que deje de ejecutarse la funcion obstacleGenerator()
+   position += gravity;
+
+
 }
 
 //Genera un enemigo Spagueti monster
@@ -146,3 +170,8 @@ function generateEnemy(){
    $(".bird").after('<div class="monster"></div>')
 }
 
+function addSpeed(){
+   var acc = 10;
+   var speedEarth = acc-0.01;
+   $("#container-floor").css({"-webkit-animation-duration": (10-speedEarth) +"s"})
+}
